463c135bd6b7d52a7b3f8643d0aaf612a0f0e5fd
Todd Resudek <info@supersimple.org>
2015-09-07 22:19:51 -0600

    adding twitter login

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,5 +1,11 @@
 class CallbacksController < Devise::OmniauthCallbacksController
+  
+  ####TODO: use dynamic methods to generate this code. ####
   def google
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+    sign_in_and_redirect @user
+  end
+  def twitter
     @user = User.from_omniauth(request.env["omniauth.auth"])
     sign_in_and_redirect @user
   end
