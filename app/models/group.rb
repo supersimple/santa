c7979d115334bc36e68d33d69b1dd28fc444c3e9
Todd Resudek <toddresudek@gmail.com>
2015-09-16 21:54:26 -0600

    defining shuffle method

--- app/models/group.rb
+++ app/models/group.rb
@@ -12,7 +12,21 @@ class Group
   attr_accessor :title, :description, :active, :user_id
   
   def shuffle_members
-    self.members
+    members = self.members
+    shuffled_members = members.shuffle
+    shifted_shuffled_members = shift_members(shuffled_members.dup)
+    @shuffled_pairs = []
+    members.each_with_index do |m, i|
+      @shuffled_pairs << {shuffled_members[i] => shifted_shuffled_members[i]}
+    end
+  end
+  
+  private
+  
+  def shift_members(arr)
+    arr.push arr.first
+    arr.shift
+    arr
   end
   
 end
